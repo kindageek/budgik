fabdc8060beecef329fd20f8280c9d8e4151dfde
Sanzhar Nussipbek <Nsanzhar.99@gmail.com>
2022-12-06 20:49:02 +0800

    feat: overview header

---
+++ src/components/overview/overview-header.component.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PageHeader from "../page-header/page-header.component";
+
+const OverviewHeader: React.FC = () => {
+  const month = new Date().toLocaleString("default", { month: "long" });
+  const year = new Date().getFullYear();
+
+  return (
+    <div className="flex w-full items-center justify-between">
+      <PageHeader title="Overview" />
+      <h3 className="whitespace-nowrap text-2xl font-medium text-gray-600">{`${month}, ${year}`}</h3>
+    </div>
+  );
+};
+
+export default OverviewHeader;
--- src/components/overview/overview.component.tsx
+++ src/components/overview/overview.component.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 
-import PageHeader from '../page-header/page-header.component';
-import PageContainer from '../page-container/page-container.component';
+import OverviewHeader from "./overview-header.component";
+import PageContainer from "../page-container/page-container.component";
 
 const Overview: React.FC = () => {
   return (
     <PageContainer>
-      <PageHeader title="Overview" />
-      
+      <OverviewHeader />
     </PageContainer>
   );
 };
