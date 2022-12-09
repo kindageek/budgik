57327d648e528aa7cbf982101fcfa1a39381dc44
Sanzhar Nussipbek <Nsanzhar.99@gmail.com>
2022-12-09 09:54:17 +0800

    fix: dashboard scroll

--- src/components/dashboard-layout/dashboard-layout.component.tsx
+++ src/components/dashboard-layout/dashboard-layout.component.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import React from "react";
 import Sidebar from "../sidebar/sidebar.component";
 import styles from "./dashboard-layout.module.css";
@@ -5,12 +6,22 @@ import styles from "./dashboard-layout.module.css";
 const DashboardLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
+  const router = useRouter();
+  const isDashboardOverview = router.pathname === "/dashboard";
   return (
     <div
-      className={`${styles.dashboardLayout} flex w-screen`}
+      className={`${
+        isDashboardOverview
+          ? styles.dashboardLayout
+          : styles.dashboardLayoutNoScroll
+      } flex w-screen`}
     >
       <Sidebar />
-      <div className={`${styles.content} h-full w-full`}>
+      <div
+        className={`${styles.content} ${
+          isDashboardOverview ? "" : styles.overflowHidden
+        } h-full w-full`}
+      >
         {children}
       </div>
     </div>
